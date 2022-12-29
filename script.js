c9ec9f871726ad0f6f8767fe07f8e4be5065f9fa
Karl Vestin <rixw@me.com>
2022-12-28 23:30:49 -0500

    Header separeted into individual divs when title scroll crosses viewport

--- script.js
+++ script.js
@@ -1,17 +1,58 @@
+// Removes preload class to body after 1/2 secs
+
 setTimeout(function(){
     document.body.className="";
 },500);
 
+// This below removed a class on load, no longer needed
+
+// window.onload = function() {
+//     krv.classList.remove('krv-unobserved');
+// };
+
+// document.onload = function () {
+//     krv.classList.remove('krv-unobserved');
+// };
 
 
-const krv = document.querySelector('.krv')
-const title = document.querySelector('.title')
-const myName = document.querySelector('.my-name')
-const sec1 = document.querySelector('.sec1')
+// 1. Get variables
 
-console.log(sec1);
+const krv = document.querySelector('.krv');
+const title = document.querySelector('.title');
+const myName = document.querySelector('.my-name');
+const sec1 = document.querySelector('.sec1');
+const krvInnerHTML = krv.innerHTML;
+const krvWords = krvInnerHTML.split("<br>");
+const krvAllCharNoSpaces = krvWords.join("");
+const krvAmountOfWords = krvWords.length;
 
+// Find out how many letters each word is
+
+let krvEachWordLength = [];
+
+for (let i = 0; i < krvAmountOfWords; i++) {
+    // console.log(krvWords[i].length);
+    krvEachWordLength.push(krvWords[i].length);
+  }
+
+  console.log(krvEachWordLength);
+
+
+// clg some stuff to check
+
+// console.log(krvInnerHTML);
 console.log(krv);
+console.log(krvWords);
+console.log(krvAmountOfWords);
+
+console.log(krvAllCharNoSpaces);
+
+// Make array of krvInnerHTML variable
+let krvArray = [...krvAllCharNoSpaces];
+let krvArrayLength = krvArray.length;
+console.log(krvArrayLength);
+
+// intersection observer for ${title} hitting 50% of viewport
 
 const titleOptions = { 
     root: null,
@@ -22,26 +63,58 @@ const titleOptions = {
 const titleObserver = new IntersectionObserver(function(entries, titleObserver) {
     
     entries.forEach(entry => {
-        console.log(entries[0]);
+        console.log(entry.target, entry.isIntersecting);
         if (entry.isIntersecting) {
-            krv.classList.add('krv-observed');
-            krv.classList.remove('krv-unobserved');
+            // Set string to blank string
+            krv.innerHTML = " ";
+            // Set different nameClass variable for each word
+            // let nameClass = " ";
+
+         
+            // Make a div for each character
+            krvArray.forEach((character, i) => {
+                // console.log(character,i);
+                let lowerCaseChar = character.toLocaleLowerCase();
+
+                if(i >= + krvAllCharNoSpaces.length - krvWords[2].length) {
+                    let nameClass = "vestin";
+                    krv.innerHTML += `<div class="krv_${nameClass}-${lowerCaseChar}">${character}</div>`;
+                }
+                else if (i < krvAllCharNoSpaces.length - (krvWords[1].length + krvWords[2].length)) {
+                    let nameClass = "karl";
+                    krv.innerHTML += `<div class="krv_${nameClass}-${lowerCaseChar}">${character}</div>`;
+                }
+                else {
+                    let nameClass = "rickard";
+                    krv.innerHTML += `<div class="krv_${nameClass}-${lowerCaseChar}">${character}</div>`;
+                }
+                
+
+            });
+
+            
+            
+        krv.classList.add('krv-observed');
+        // krv.innerHTML = krvArray;
+            // console.log(krv);
         } else {
             krv.classList.remove('krv-observed');
-            krv.classList.add('krv-unobserved');
+            krv.innerHTML = krvInnerHTML;
+            // console.log(krv.innerHTML);
         }
     });
 }, titleOptions);
 
 titleObserver.observe(title);
 
-window.onload = function() {
-    krv.classList.remove('krv-unobserved');
-};
 
-document.onload = function () {
-    krv.classList.remove('krv-unobserved');
-};
+
+
+
+
+
+
+// Old stuff below
 
 // OBSERVER FOR SECTIONS AND STICKY HEADER BORDER BOTTOM CLASS
 
