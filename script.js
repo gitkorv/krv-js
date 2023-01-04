43373e4c979d27fb8a9a3db3c96a6bfdac27c2e9
Karl Vestin <rixw@me.com>
2023-01-04 12:12:46 -0500

    header animation fixed so it wont yank

--- script.js
+++ script.js
@@ -18,39 +18,67 @@ setTimeout(function(){
 // 1. Get variables
 
 const krv = document.querySelector('.krv');
+    console.log(krv);
+
+const krvInnerHTML = krv.innerHTML;
+    console.log(krvInnerHTML);
+
+const krvOuterHTML = krv.outerHTML;
+console.log(krvOuterHTML);
+
+const krvWords = krvInnerHTML.split("<br>");
+    console.log(krvWords);
+
+    const krvString = krvWords.join(" ");
+console.log(krvString);
+
+const krvAllCharNoSpaces = krvWords.join("");
+    console.log(krvAllCharNoSpaces);
+
+// Make array of krvInnerHTML
+let krvArray = [...krvAllCharNoSpaces];
+console.log(krvArray);
+
+// Get krv total length
+let krvTotalLength = krvArray.length;
+
+
 const title = document.querySelector('.title');
 const myName = document.querySelector('.my-name');
 const sec1 = document.querySelector('.sec1');
-const krvInnerHTML = krv.innerHTML;
-const krvWords = krvInnerHTML.split("<br>");
-const krvAllCharNoSpaces = krvWords.join("");
 const krvAmountOfWords = krvWords.length;
 
 // Find out how many letters each word is
 
-let krvEachWordLength = [];
+const krvEachWordLength = krv.innerHTML.split("<br>").map(w => w.length);
 
-for (let i = 0; i < krvAmountOfWords; i++) {
-    // console.log(krvWords[i].length);
-    krvEachWordLength.push(krvWords[i].length);
+
+
+console.log("Below here I try to figure out how to grab specific letters");
+
+// First find out what each words first letter is
+
+const getFirstLetters = function(array) {
+    const firstLetters = array.map(word => word[0]);
+  
+    return firstLetters;
   }
+  
+console.log(getFirstLetters(krvWords));
 
-  console.log(krvEachWordLength);
+// Get index of each words first letter in krvAllCharNoSpaces
+
+const firstWordIndexes = [];
+
+for (let i = 0; i < krvAllCharNoSpaces.length; i++) {
+  if (krvAllCharNoSpaces[i] === getFirstLetters(krvWords)[0] || krvAllCharNoSpaces[i] === getFirstLetters(krvWords)[1] || krvAllCharNoSpaces[i] === getFirstLetters(krvWords)[2]) {
+    firstWordIndexes.push(i);
+  }
+}
+console.log(firstWordIndexes); 
 
 
-// clg some stuff to check
 
-// console.log(krvInnerHTML);
-console.log(krv);
-console.log(krvWords);
-console.log(krvAmountOfWords);
-
-console.log(krvAllCharNoSpaces);
-
-// Make array of krvInnerHTML variable
-let krvArray = [...krvAllCharNoSpaces];
-let krvArrayLength = krvArray.length;
-console.log(krvArrayLength);
 
 // intersection observer for ${title} hitting 50% of viewport
 
@@ -65,42 +93,56 @@ const titleObserver = new IntersectionObserver(function(entries, titleObserver) 
     entries.forEach(entry => {
         console.log(entry.target, entry.isIntersecting);
         if (entry.isIntersecting) {
-            // Set string to blank string
+            // Set krv innerHTML string to blank 
             krv.innerHTML = " ";
-            // Set different nameClass variable for each word
-            // let nameClass = " ";
-
-         
-            // Make a div for each character
+           
+            // Make a div with specific classes for each character
             krvArray.forEach((character, i) => {
                 // console.log(character,i);
-                let lowerCaseChar = character.toLocaleLowerCase();
+                let lowerCaseChar = character.toLowerCase();
 
-                if(i >= + krvAllCharNoSpaces.length - krvWords[2].length) {
+                if(i >= + krvTotalLength - krvWords[2].length) {
                     let nameClass = "vestin";
-                    krv.innerHTML += `<div class="krv_${nameClass}-${lowerCaseChar}">${character}</div>`;
+                    krv.innerHTML += `<div class="krv_${nameClass}-${lowerCaseChar} krv${i}">${character}</div>`;
                 }
-                else if (i < krvAllCharNoSpaces.length - (krvWords[1].length + krvWords[2].length)) {
+                else if (i < krvTotalLength - (krvWords[1].length + krvWords[2].length)) {
                     let nameClass = "karl";
-                    krv.innerHTML += `<div class="krv_${nameClass}-${lowerCaseChar}">${character}</div>`;
+                    krv.innerHTML += `<div class="krv_${nameClass}-${lowerCaseChar} krv${i}">${character}</div>`;
                 }
                 else {
                     let nameClass = "rickard";
-                    krv.innerHTML += `<div class="krv_${nameClass}-${lowerCaseChar}">${character}</div>`;
+                    krv.innerHTML += `<div class="krv_${nameClass}-${lowerCaseChar} krv${i}">${character}</div>`;
                 }
-                
-
             });
 
-            
-            
-        krv.classList.add('krv-observed');
-        // krv.innerHTML = krvArray;
-            // console.log(krv);
+
+        // Get K R V elements
+        const krvDivs = document.querySelectorAll(".krv div");
+        console.log(krvDivs[0]);
+        krvDivs.forEach((entry, i) => {
+            entry.classList.add('hidden')
+        });
+
+        const karlK = document.querySelector(".krv0");
+        const rickardR = document.querySelector(".krv4");
+        const vestinV = document.querySelector(".krv11");
+
+        // Add animation (observed) class
+        krv.classList.add('krv-observed')
+
+        karlK.classList.remove('hidden');
+        karlK.innerHTML = karlK.innerHTML.toLowerCase()+".";
+        rickardR.classList.remove('hidden');
+        rickardR.innerHTML = rickardR.innerHTML.toLowerCase()+".";
+        vestinV.classList.remove('hidden');
+        vestinV.innerHTML = vestinV.innerHTML.toLowerCase();
+
+        // karlK.classList.add('krv-observed');
+        
         } else {
+            // Remove animation (observed) class
             krv.classList.remove('krv-observed');
             krv.innerHTML = krvInnerHTML;
-            // console.log(krv.innerHTML);
         }
     });
 }, titleOptions);
